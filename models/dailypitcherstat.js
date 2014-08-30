f4e00adb1c63c850e2eaf4ac76ae60c733b5e29c
seanbrookes <seanbrookes@beachair.ca>
2014-08-30 00:25:12 -0700

    fix unique player id trade issue
    - added date filter to only use current stat
    updates before trying to compare mlbid so
    as to not get tripped up by older references to players
    that are no longer on the roster

--- models/dailypitcherstat.js
+++ models/dailypitcherstat.js
@@ -24,9 +24,19 @@ DailyPitcherStat.afterRemote('find', function(ctx, user, next) {
 
     for (var i = 0;i < internalResult.length;i++){
       var tPlayer = internalResult[i];
-      if (uniqueMLBIDArray.indexOf(tPlayer.mlbid) === -1){
-        uniqueMLBIDArray.push(tPlayer.mlbid);
-        filteredOut.push(tPlayer);
+
+      var playerMonth = new Date(tPlayer.date).getMonth();
+      var playerDate = new Date(tPlayer.date).getDate();
+      var currMonth = new Date().getMonth();
+      var currDate = new Date().getDate();
+
+      if (parseInt(playerMonth) >= parseInt(currMonth)) {
+        if (parseInt(playerDate) >= parseInt(currDate)) {
+          if (uniqueMLBIDArray.indexOf(tPlayer.mlbid) === -1){
+            uniqueMLBIDArray.push(tPlayer.mlbid);
+            filteredOut.push(tPlayer);
+          }
+        }
       }
     }
     ctx.result = filteredOut;
